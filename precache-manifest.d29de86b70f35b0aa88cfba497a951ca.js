35a712a5b1b72fff93f35c3de68ba5aa20f18ac7
DaeWon <cskang8264@naver.com>
2019-03-17 20:51:03 +0900

    Updates

---
+++ precache-manifest.d29de86b70f35b0aa88cfba497a951ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a45f35e59690beb96a47",
+    "url": "/movie_app/static/css/main.d470732f.chunk.css"
+  },
+  {
+    "revision": "a45f35e59690beb96a47",
+    "url": "/movie_app/static/js/main.2f9382c6.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  },
+  {
+    "revision": "79876ddfb540893b229e",
+    "url": "/movie_app/static/js/2.28db7df0.chunk.js"
+  },
+  {
+    "revision": "5a72750113426c1799a8cb76da142801",
+    "url": "/movie_app/index.html"
+  }
+];
