d3bf922fe3cc5df67976446d4ea31582857cefdb
DaeWon <cskang8264@naver.com>
2019-03-29 11:55:29 +0900

    reactMovieApp

--- src/App.js
+++ src/App.js
@@ -4,13 +4,61 @@ import './App.css';
 import Movie from './Movie';
 
 
+
+
+
+
+
 class App extends Component {
+  //Render : componentWllMount() -> render() -> componentDidMount()
+  //Update componentWillReceiveProps() - > shouldComponentUpdate() - >componentWillUpdate() -> render() > component
+
+  state = {}
+
+  componentDidMount() {
+    this._getMovies();
+
+  }
+
+  _renderMovies = () => {   //렌더 ,로딩기능 밑에도 있음.
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+
+      return <Movie
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres} 
+        synopsis={movie.synopsis}
+
+        /> 
+         
+    })
+    return movies
+
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi() //_callApi가 끝나기를 기다리는것, 성공적으로 수행이아니라. 그래서 _callApi가 실행되고 밑에 setState 수행됨.
+
+    this.setState({
+      movies
+    })
+  }
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
+
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-       hello!
-       <Movie />
-       
+      <div className={movies ? "App" : "App--loading"}>
+
+        {movies ? this._renderMovies() : 'Loading'}
+
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,25 +1,59 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
-
-class Movie extends Component {
-    render(){
-        return(
-            <div>
-                <Movieposter />
-                 <h1>hello react</h1>
-
+function Movie({title, poster, genres, synopsis}){
+    return (
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title} />
+            </div>   
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Movie__Synopsis">
+                <LinesEllipsis
+                    text={synopsis}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                    />   
+                </div>
             </div>
-           
-         )
-    } 
+        </div>
+    )
 }
-class Movieposter extends Component{
-    render(){
-        return(
-            <img src="https://t1.daumcdn.net/cfile/tistory/99D7C7335C51249D30"/>
-        )
-    }
+
+function MoviePoster({poster, alt}){
+    return (
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
 }
- 
-export default Movie;
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes ={
+    genre: PropTypes.string.isRequired
+}
+
+export default Movie
